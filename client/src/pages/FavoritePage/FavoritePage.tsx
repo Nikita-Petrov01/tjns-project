0b4c1bff1772536ccdca94489635e09538ed5e98
Nuri-engineer <abdurahmon_1989@mail.ru>
2025-04-30 14:31:20 +0300

    rel favPage

---
+++ client/src/pages/FavoritePage/FavoritePage.tsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import { useAppDispatch } from '../../shared/lib/hooks';
+
+// export default function FavoritePage(): React.JSX.Element {
+//   const dispatch = useAppDispatch();
+//   const favorite = useAppSelector((state) => )
+//   return (
+//     <div className="col-12" key={item.id}>
+//       <CartItemCard
+//         image={product.images[0]}
+//         name={product.name}
+//         price={item.price}
+//         quantity={item.quantity}
+//         stock={product.stock}
+//         add={() => handleAdd(item.productId)}
+//         remove={() => handleRemove(item.productId)}
+//         onDelete={() => handleDelete(item.productId)}
+//         isOutOfStock={isOutOfStock}
+//       />
+//     </div>
+//   );
+// }
--- server/src/routes/searchRouter.js
+++ server/src/routes/searchRouter.js
@@ -6,7 +6,7 @@ const { Op } = require('sequelize');
 router.get('/', async (req, res) => {
   try {
     const { query, categoryName, limit = 10, offset = 0 } = req.query;
-    
+
     if (!query && !categoryName) {
       return res.status(400).json({
         success: false,
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
       });
     }
 
-    const where = {}
+    const where = {};
 
     if (query) {
       where.name = {
@@ -34,58 +34,25 @@ router.get('/', async (req, res) => {
       where.categoryId = category.id;
     }
 
-    const products = await Product.findAll({ where, 
-      limit: parseInt(limit),
-      offset: parseInt(offset),
-      include: [{model: Category}],
+    const products = await Product.findAll({
+      where,
+      limit: parseInt(limit, 10),
+      offset: parseInt(offset, 10),
+      include: [{ model: Category }],
       order: [['createdAt', 'DESC']],
-     });
+    });
 
-    
     return res.status(200).json({
       success: true,
-      results:products,
+      results: products,
       message: 'Поиск выполнен успешно',
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ success: false, error: `Ошибка сервера: ${error.message}` });
+    return res
+      .status(500)
+      .json({ success: false, error: `Ошибка сервера: ${error.message}` });
   }
 });
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const { categoryName } = req.query;
-//     if (!categoryName) {
-//       return res.status(400).json({
-//         success: false,
-//         message: 'Поле categoryName не может быть пустым',
-//       });
-//     }
-
-//     const category = await Category.findOne({ where: { name: categoryName } });
-//     if (!category) {
-//       return res.status(404).json({
-//         success: false,
-//         message: 'Категория не найдена',
-//       });
-//     }
-
-//     const products = await Product.findAll({
-//       where: {
-//         categoryId: category.id,
-//       },
-//     });
-
-//     res.status(200).json({
-//       success: true,
-//       results: products,
-//       message: 'Поиск выполнен успешно',
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ success: false, error: `Ошибка сервера: ${error.message}` });
-//   }
-// });
 
 module.exports = router;
